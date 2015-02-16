e6d1121e30b4c6b365defeb8fb847a81cb7a6422
geogaffer <jharrop@acm.org>
2015-02-15 23:19:51 -0800

    updates to download group of functions.  The full historical data for players can be downloaded and is saved to a playerlist table and to individual tables for each player.  Currently 678 players in the League.

--- download.R
+++ download.R
@@ -1,4 +1,4 @@
-getPlayerData <- function(playerNum=180) {
+download.player <- function(playerNum=180) {
     
     library(jsonlite)
     
@@ -9,23 +9,56 @@ getPlayerData <- function(playerNum=180) {
     jsonData
 }
 
-getPlayerHistory <- function(playerNum=180) {
+download.history <- function(playerNum=180) {
     
     library(jsonlite)
+    
+    if (!file.exists("./data raw")) { dir.create("./data raw") }
     
     playerURL = paste("http://fantasy.premierleague.com/web/api/elements/", 
                       playerNum, '/', sep="")
     jsonData <- fromJSON(playerURL)
     
-    jsonData$fixture_history
+    history.all <- jsonData$fixture_history$all
+    history.all <- as.data.frame(history.all, stringsAsFactors=FALSE)
+    names(history.all) <- c("date", "week", "result", "minutes", "goals", "assists", 
+                            "clean", "conceded", "own", "pen save", "pen miss", 
+                            "yellows", "reds", "saves", "bonus", "ppi", "bps",
+                            "transfers", "value", "points")
+    for (i in c(2, 4:20)) {
+        history.all[,i] <- as.integer(history.all[,i])
+    }
+    opponent <- substr(history.all$result, 1, 3)
+    hora <- substr(history.all$result, 5, 5)
+    home <- (hora=="H")
+    
+    us <- vector(mode="integer", length=length(home))
+    them <- vector(mode="integer", length=length(home))
+    for (i in seq(1:length(home))) {
+        if (home[i]) {
+            us[i] <- as.integer(substr(history.all$result[i], 7, 8))
+            them[i] <- as.integer(substr(history.all$result[i], 10, 11))
+        } else {
+            them[i] <- as.integer(substr(history.all$result[i], 7, 8))
+            us[i] <- as.integer(substr(history.all$result[i], 10, 11))
+        }
+    }
+    
+    history.new <- cbind(history.all[,1:3], opponent, home, us, them, history.all[,4:20])
+    
+    csvName <- paste("./data raw/", playerNum, ".csv", sep="")
+    print(paste("writing... ", csvName))
+    write.csv(history.new, file=csvName)
+    history.new
 }
 
-buildPlayerList <- function(playerRange = seq(1, 15)) {
+download.playerDB <- function(playerRange = seq(1, 15)) {
     
     library(jsonlite)
     
     baseURL <- "http://fantasy.premierleague.com/web/api/elements/"
     playerID <- vector(mode="integer", length=length(playerRange))
+    photo <- vector(mode="character", length=length(playerRange))
     lastName <- vector(mode="character", length=length(playerRange))
     firstName <- vector(mode="character", length=length(playerRange))
     position <- vector(mode="character", length=length(playerRange))
@@ -45,6 +78,7 @@ buildPlayerList <- function(playerRange = seq(1, 15)) {
             playerID[i] <<- playerNum
             lastName[i] <<- second_name
             firstName[i] <<- first_name
+            photo[i] <<- photo
             position[i] <<- type_name
             team[i] <<- team_name
             totalPoints[i] <<- total_points
@@ -52,10 +86,45 @@ buildPlayerList <- function(playerRange = seq(1, 15)) {
             minutes[i] <<- minutes
         })
         
+        history.all <- jsonData$fixture_history$all
+        history.all <- as.data.frame(history.all, stringsAsFactors=FALSE)
+        names(history.all) <- c("date", "week", "result", "minutes", "goals", "assists", 
+                                "clean", "conceded", "own", "pen save", "pen miss", 
+                                "yellows", "reds", "saves", "bonus", "ppi", "bps",
+                                "transfers", "value", "points")
+        for (j in c(2, 4:20)) {
+            history.all[,j] <- as.integer(history.all[,j])
+        }
+        opponent <- substr(history.all$result, 1, 3)
+        hora <- substr(history.all$result, 5, 5)
+        home <- (hora=="H")
+        
+        us <- vector(mode="integer", length=length(home))
+        them <- vector(mode="integer", length=length(home))
+        for (k in seq(1:length(home))) {
+            if (home[k]) {
+                us[k] <- as.integer(substr(history.all$result[k], 7, 8))
+                them[k] <- as.integer(substr(history.all$result[k], 10, 11))
+            } else {
+                them[k] <- as.integer(substr(history.all$result[k], 7, 8))
+                us[k] <- as.integer(substr(history.all$result[k], 10, 11))
+            }
+        }
+        
+        history.new <- cbind(history.all[,1:3], opponent, home, us, them, history.all[,4:20])
+        
+        csvName <- paste("./data raw/", playerNum, ".csv", sep="")
+        print(paste("writing... ", csvName))
+        write.csv(history.new, file=csvName)
+        
+        #print(i)
+        Sys.sleep(5)
     }
-    playerList <- cbind(playerID, lastName, firstName, position, team,
+    
+    playerList <- cbind(playerID, lastName, firstName, photo, position, team,
                         totalPoints, cost, minutes)
-    names(playerList) <- c("playerID", "lastName", "firstName", "position", "team",
-                           "totalPoints", "cost", "minutes")
+    names(playerList) <- c("playerID", "lastName", "firstName", "photo", "position", 
+                           "team", "totalPoints", "cost", "minutes")
+    
     playerList
 }
